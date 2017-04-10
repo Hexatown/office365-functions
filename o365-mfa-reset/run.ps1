61eed0f14946b8fa66187f02999744bb438eeb68
Flamur Mavraj <fmavr@nets.eu>
2017-04-10 11:14:55 +0200

    Added input & output files

--- o365-mfa-reset/run.ps1
+++ o365-mfa-reset/run.ps1
@@ -1,3 +1,10 @@
+
+if ($triggerInput -eq $null){
+    $out = "$PSScriptRoot\output.json"
+    $triggerInput = "$PSScriptRoot\input.json"
+}
+
+
 $input = Get-Content $triggerInput -Raw
 $request =  ConvertFrom-Json -InputObject $input
 
