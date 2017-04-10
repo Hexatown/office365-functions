1e7f5538f7d06b154161350f25fc81b8d512dfa0
Flamur Mavraj <fmavr@nets.eu>
2017-04-10 15:03:09 +0200

    ...

--- o365-mfa-reset/run.ps1
+++ o365-mfa-reset/run.ps1
@@ -1,53 +1,15 @@
+# https://david-obrien.net/2016/07/azure-functions-PowerShell/
+# https://david-obrien.net/2016/11/azure-functions-byo-powershell-modules/
+# http://blog.tyang.org/2016/10/07/using-custom-powershell-modules-in-azure-functions/
+
 if ($triggerInput -eq $null){
     $res = "$PSScriptRoot\output.json"
     $triggerInput = "$PSScriptRoot\input.json"
 }
 
+
 $req = Get-Content $triggerInput -Raw
-$params =  ConvertFrom-Json -InputObject $req
-$upn = $params.body.upn
+$requestBody =  ConvertFrom-Json -InputObject $req
+$name = $requestBody.body.upn
 
-if($upn -eq $null){
-
-    exit
-}
-
-
-param (
-    [Parameter(Mandatory = $true)]
-    [string] $upn
-)
-
-Function O365-MFA-Exists{
-    param
-    (
-        [Object] $msoluser
-    )
-
-    return $msoluser | Where-Object {$_.StrongAuthenticationRequirements -like '*'}
-}
-
-Function O365-MFA-Reset{
-    param
-    (
-        [Object] $upn
-    )
-
-    $result = @{}
-
-
-    $msoluser = Get-MsolUser -UserPrincipalName $upn -ErrorAction SilentlyContinue
-
-    if(O365-MFA-Exists $msoluser){
-        Set-MsolUser -UserPrincipalName $upn -StrongAuthenticationMethods @()
-        $result.message = 'MFA reset'
-    }else{
-        $result.message = 'MFA is not enabled for this user'
-        $result.error = $true
-    }
-
-    $json = convertto-json -InputObject $result
-    Out-File -Encoding Ascii -FilePath $res -inputObject $json
-}
-
-O365-MFA-Reset -upn $upn
+Write-Output "Resetting password for $name"
