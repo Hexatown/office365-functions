85477a4505b3d3b171df78330857119dd5fb2015
Flamur Mavraj <fmavr@nets.eu>
2017-04-10 14:30:24 +0200

    ...

--- o365-mfa-reset/run.ps1
+++ o365-mfa-reset/run.ps1
@@ -1,11 +1,3 @@
-if ($triggerInput -eq $null){
-    $res = "$PSScriptRoot\output.json"
-    $triggerInput = "$PSScriptRoot\input.json"
-}
 
 
-$req = Get-Content $triggerInput -Raw
-$requestBody =  ConvertFrom-Json -InputObject $req
-$name = $requestBody.body.mailbox
-
-Write-Output "Resetting password for $name"
+Write-Output $PSScriptRoot
