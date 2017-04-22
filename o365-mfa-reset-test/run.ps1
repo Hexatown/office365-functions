a2914170d8cf83ba9374c4df3c12d31c3e71a3de
Flamur Mavraj <flamur.mavraj@ornio.no>
2017-04-22 13:41:20 +0200

    testing manual trigger

--- o365-mfa-reset-test/run.ps1
+++ o365-mfa-reset-test/run.ps1
@@ -2,18 +2,12 @@
 # https://david-obrien.net/2016/11/azure-functions-byo-powershell-modules/
 # http://blog.tyang.org/2016/10/07/using-custom-powershell-modules-in-azure-functions/
 
-if ($triggerInput -eq $null){
+if ($req -eq $null){
     $res = "$PSScriptRoot\output.json"
-    $triggerInput = "$PSScriptRoot\input.json"
+    $req = "$PSScriptRoot\input.json"
 }
 
-Write-Output $triggerInput
-
-$req = Get-Content $triggerInput -Raw
-Write-Output $req
-
-$requestBody =  ConvertFrom-Json -InputObject $req
-Write-Output $requestBody
-
+$requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $name = $requestBody.body.upn
+
 Write-Output "Resetting password for $name"
