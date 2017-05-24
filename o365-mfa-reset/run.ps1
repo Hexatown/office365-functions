28593bf248accd965da59de82699be2d53bcf843
Flamur Mavraj <flamur.mavraj@ornio.no>
2017-05-24 15:17:21 +0200

    ...

--- o365-mfa-reset/run.ps1
+++ o365-mfa-reset/run.ps1
@@ -1,13 +1,17 @@
 # Get request data and meta
-$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+$body = Get-Content $req -Raw | ConvertFrom-Json
 $data = $requestBody.data
 $meta = $requestBody.meta
 
 $result = @{}
+$result.id = $body.id
+$output.RowKey = $body.id
+$output.PartitionKey = $data.type
 $result.requester = $meta.requester
 $result.type = $data.type
 $result.email = $data.attributes.email
 
 $json = ConvertTo-Json -InputObject $result
+Write-Output $json
 
 Out-File -Encoding Ascii -FilePath $res -inputObject $json
