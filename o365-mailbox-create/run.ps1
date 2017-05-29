0d31352623599ea9facda217a514b7f83c398545
Flamur Mavraj <fmavr@nets.eu>
2017-05-29 15:07:39 +0200

    Added Office 365 Connect function again

--- o365-mailbox-create/run.ps1
+++ o365-mailbox-create/run.ps1
@@ -1,6 +1,11 @@
-####################
-# Params           #
-####################
+#
+# Init         
+#
+O365-Connect -upn $env:O365_ADMIN_UPN -secureString $env:O365_ADMIN_PWD
+
+#
+# Query params           
+#
 
 $body = Get-Content $req -Raw | ConvertFrom-Json
 $data = $body.data
@@ -24,3 +29,45 @@ Write-Output $json
 Out-File -Encoding Ascii -FilePath $res -inputObject $json
 
 
+#
+# Common functions 
+#
+
+Function O365-Connect{
+
+    param(
+        [String] $upn,
+        [String] $pwd
+    )
+
+	Import-Module MSOnline
+    Try{
+        # Using SecureString instead
+
+        $pass = ConvertTo-SecureString $pwd -AsPlainText -Force
+        #$pass = convertto-securestring -String $secureString
+        $cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $upn,$pass
+        Connect-MSOLService -Credential $cred
+
+        # Check if ImportSession exists
+        if (-not (Get-Command Get-Mailbox*) ){
+            $sessionOption = New-PSSessionOption -SkipRevocationCheck
+            $session = New-PSSession -ConfigurationName 'Microsoft.Exchange' -ConnectionUri 'https://ps.outlook.com/PowerShell-LiveID?PSVersion=3.0' -Credential $Cred -Authentication Basic -AllowRedirection -SessionOption $sessionOption
+            Import-PSSession $session -AllowClobber -DisableNameChecking | Out-Null
+
+            #Enable-PSremoting -force
+        }
+    }Catch{
+        $e = $_.Exception;
+        $eMessage = $e.Message
+        $eItem = $e.ItemName
+
+        Write-Output '==========================='
+		Write-Output 'Error:'
+		Write-Output $('Message: '+$eMessage)
+		Write-Output $('Item: '+$eItem)
+		Write-Output '==========================='
+
+        exit
+    }
+}
